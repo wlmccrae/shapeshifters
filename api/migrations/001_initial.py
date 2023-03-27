41010ff35de3e9e3bdcdf2ca1283f0d783d8dac2
512kma <makane89@gmail.com>
2023-03-27 15:31:43 -0500

    created Postgres db

---
+++ api/migrations/001_initial.py
@@ -0,0 +1,60 @@
+steps = [
+    ############## USERS ##############
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY NOT NULL,
+            first_name VARCHAR(100) NOT NULL,
+            last_name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(12) NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE users;
+        """
+    ],
+    ############## EVENTS ##############
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE events (
+            id SERIAL PRIMARY KEY NOT NULL,
+            host_id INT NOT NULL, FOREIGN KEY(host_id) REFERENCES users(id),
+            host_email VARCHAR(100) NOT NULL,
+            event_name VARCHAR(250) NOT NULL,
+            event_type VARCHAR(100) NOT NULL,
+            image_url VARCHAR(500),
+            start_datetime TIMESTAMP NOT NULL,
+            end_datetime TIMESTAMP NOT NULL,
+            address_line1 VARCHAR(250) NOT NULL,
+            address_line2 VARCHAR(250),
+            city VARCHAR(100) NOT NULL,
+            state VARCHAR(100) NOT NULL,
+            zip VARCHAR(100) NOT NULL,
+            country VARCHAR(100) NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE events;
+        """
+    ],
+    ############## ATTENDEES ##############
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE attendees (
+            id SERIAL PRIMARY KEY NOT NULL,
+            host_id INT NOT NULL, FOREIGN KEY(host_id) REFERENCES users(id),
+            event_id INT NOT NULL, FOREIGN KEY(event_id) REFERENCES events(id)
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE attendees;
+        """
+    ]
+]
