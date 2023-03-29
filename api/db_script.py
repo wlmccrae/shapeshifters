57ec7fcdb2359e9fb6a88c7f67819dd963be1178
Emily Arai <emilyarai@gmail.com>
2023-03-29 16:48:20 -0400

    Adding db_script.py with notes from Candace

---
+++ api/db_script.py
@@ -0,0 +1,4 @@
+# create a few different users
+# import AccountsQueries and use create method
+# pick off the ids for those uses
+# take those ids and plug them in when you're seeding the database for the events
