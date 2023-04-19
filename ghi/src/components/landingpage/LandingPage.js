cf45b6f03a8c2e3bfb6b06cb7d90a63017831d61
Wanda McCrae <wanda.mccrae@gmail.com>
2023-04-19 12:51:04 -0400

    conditional React pages for LoggedIn and not Logged in

--- ghi/src/components/landingpage/LandingPage.js
+++ ghi/src/components/landingpage/LandingPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
+import { useGetAccountQuery } from '../../services/auth';
 import './LandingPage.css';
 
 import skatingImg from '../../imgs/091108nyc096-nw.jpg';
@@ -28,6 +29,17 @@ function LandingPage() {
     ];
     const [currentIndex, setCurrentIndex] = useState(0);
 
+    const { data: account } = useGetAccountQuery();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            nextSlide();
+        },5000);
+
+        return () => clearTimeout(timer);
+
+    }, [currentIndex]);
+
     const prevSlide =() => {
         const isFirstSlide = currentIndex === 0;
         const newIndex = isFirstSlide ? slides.length - 1: currentIndex -1;
@@ -44,36 +56,46 @@ function LandingPage() {
         setCurrentIndex(slideIndex);
     }
 
+    const loggedIn = () => (
+        <>
+            <h1> Logged In View </h1>
+        </>
+    );
 
-    return (
-        <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
-            <h1> Organize Your Next Workout Event with ShapeMates! </h1>
+    const notLoggedIn = () => (
+        <>
+            <div className='max-w-[1400px] w-full m-auto py-10 px-4 relative'>
+                <h1> Organize Your Next Workout Event with ShapeShifters! </h1>
+            </div>
+            <div className='max-w-[800px] h-[600px] w-full m-auto py-1 px-10 relative group'>
+                <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500 drop-shadow-2xl'>
+                </div>
 
-            <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500 drop-shadow-2xl'>
-            </div>
+                <div className='hidden group-hover:block absolute top-[70%] -translate-x-0 translate-y-[-75%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                    <BsChevronCompactLeft onClick={prevSlide} size={30} />
+                </div>
 
-            <div className='hidden group-hover:block absolute top-[70%] -translate-x-0 translate-y-[-75%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                <BsChevronCompactLeft onClick={prevSlide} size={30} />
-            </div>
-            <div className='hidden group-hover:block absolute top-[70%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                <BsChevronCompactRight onClick={nextSlide} size={30} />
-            </div>
-            <div className='flex top-4 justify-center py-2'>
-                {slides.map((slide, slideIndex) => (
-                    <div key={slideIndex} onClick={() => goToSlide(slideIndex)} className= 'text-2xl cursor-pointer'><RxDotFilled /> </div>
-                ))}
-            </div>
-            <div className='flex'>
-                <div className='flex-none w-1/2 mr-3'>
+                <div className='hidden group-hover:block absolute top-[70%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                    <BsChevronCompactRight onClick={nextSlide} size={30} />
+                </div>
+
+                <div className='flex top-4 justify-center py-2'>
+                    {slides.map((slide, slideIndex) => (
+                        <div key={slideIndex} onClick={() => goToSlide(slideIndex)} className= 'text-2xl cursor-pointer'><RxDotFilled /> </div>
+                    ))}
+                </div>
+                <div className='flex-none w-full mr-3'>
                     <h2>Want to host a hiking trip, find a new spotter, or join a group fitness class?</h2>
                     <p>ShapeShifters makes it easy to find ShapeMates to help you make the next step in your journey. Reach a wider audience and bring people together around a shared passion for fitness today.</p>
                 </div>
-                <div className='flex-none w-1/2 ml-3'>
-                    <h2> Search Capability TBA </h2>
-                </div>
             </div>
-        </div>
+        </>
+    );
 
+    return (
+        <>
+            {account ? loggedIn() : notLoggedIn()}
+        </>
     );
 };
 
