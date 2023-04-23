853342b5cc919d22aa1eade75732d70377db0c8c
Emily Arai <emilyarai@gmail.com>
2023-04-23 07:26:28 -0400

    API key import not working

--- ghi/src/components/EventMap.js
+++ ghi/src/components/EventMap.js
@@ -8,12 +8,18 @@ import Modal from "./Modal";
 import { hideEventMapModal } from "../features/events/eventMapSlice";
 import { useGetEventQuery } from "../services/events";
 
-const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-console.log(GOOGLE_API_KEY);
+import GOOGLE_MAP_API_KEY from "../keys"
+//Still need help with hiding the API key
+// const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+console.log(GOOGLE_MAP_API_KEY);
 const containerStyle = {
   width: "400px",
   height: "400px",
 };
+
+// This variable from react google maps is now in the eventMapSlice
+// as the initialState. We pass it as a prop to the EventMap component
+// in the component where the EventMap Modal is rendered
 
 // const center = {
 //   lat: -3.745,
@@ -22,23 +28,28 @@ const containerStyle = {
 
 function EventMap({ center }) {
   const dispatch = useDispatch();
-
+  // Get the eventMapModal for the Modal Component
   const { eventMapModal } = useSelector((state) => state.eventMap);
-  //   console.log("CENTER", center)
+
+  // Logic taken from react-google-maps: https://www.npmjs.com/package/@react-google-maps/api
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: "AIzaSyACbmfzsE_62WbilmejtzcDzcWq8R5gchk",
+    googleMapsApiKey: GOOGLE_MAP_API_KEY,
   });
+
+  // Set the location of the marker
   const markerLocation = useMemo(() => ({
     lat: center?.lat,
     lng: center?.lng,
   }));
 
+  // lines 40-54 are taken from react-google-maps
   const [map, setMap] = React.useState(null);
 
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
+    // had to comment out map.fitBounds(bounds) to get MarkerF to work
     // map.fitBounds(bounds);
 
     setMap(map);
@@ -48,25 +59,20 @@ function EventMap({ center }) {
     setMap(null);
   }, []);
 
-  //   // Query /api/event/{event_id} for the event with the id from the payload
-  //   const { data, isLoading } = useGetEventQuery(eventId.payload);
-  //   if (isLoading) return <div>Loading...</div>;
-
   return isLoaded ? (
     <Modal
       visible={eventMapModal}
       onClose={() => dispatch(hideEventMapModal())}
     >
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={12}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
-        >
-          {/* Child components, such as markers, info windows, etc. */}
-          <MarkerF position={center} />
-        </GoogleMap>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={12}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        <MarkerF position={center} />
+      </GoogleMap>
     </Modal>
   ) : (
     <></>
