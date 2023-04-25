64d43a5225a0908f6dc201099aa6951112ffe15d
Wanda McCrae <wanda.mccrae@gmail.com>
2023-04-25 10:32:40 -0400

    Tab color changes depending on which list of events is shown

--- ghi/src/components/eventspage/EventsPageTabs.js
+++ ghi/src/components/eventspage/EventsPageTabs.js
@@ -6,6 +6,9 @@ import { showHostingEvents, showAttendingEvents } from '../../features/events/ev
 
 function EventsPageTabs() {
     const dispatch = useDispatch();
+    const { userRole } = useSelector(
+        (state) => state.eventsPage
+    );
 
     const handleAttendingClick = (e) => {
         dispatch(showAttendingEvents());
@@ -15,27 +18,54 @@ function EventsPageTabs() {
         dispatch(showHostingEvents());
     }
 
+    const attending = () => (
+        <>
+            <li className="-mb-px mr-1">
+                <NavLink
+                    onClick={handleAttendingClick}
+                    className="bg-jet-stream-900 inline-block border-l border-t border-r rounded-t py-2 px-4 text-jet-stream-500 hover:text-morning-glory-700 font-semibold hover:bg-jet-stream-200 active:bg-jet-stream-900
+                    active:text-jet-stream-500"
+                    to="/events/attending">
+                        Attending
+                </NavLink>
+            </li>
+            <li className="mr-1">
+                <NavLink
+                    onClick={handleHostingClick}
+                    className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-jet-stream-900 hover:text-morning-glory-700 font-semibold hover:bg-jet-stream-200 active:bg-jet-stream-900 active:text-jet-stream-500"
+                    to="/events/hosting">
+                        Hosting
+                </NavLink>
+            </li>
+        </>
+    );
+
+    const hosting = () => (
+        <>
+            <li className="-mb-px mr-1">
+                <NavLink
+                    onClick={handleAttendingClick}
+                    className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-jet-stream-900 hover:text-morning-glory-700 font-semibold hover:bg-jet-stream-200 active:bg-jet-stream-900
+                    active:text-jet-stream-500"
+                    to="/events/attending">
+                        Attending
+                </NavLink>
+            </li>
+            <li className="mr-1">
+                <NavLink
+                    onClick={handleHostingClick}
+                    className="bg-jet-stream-900 inline-block border-l border-t border-r rounded-t py-2 px-4 text-jet-stream-500 hover:text-morning-glory-700 font-semibold hover:bg-jet-stream-200 active:bg-jet-stream-900 active:text-jet-stream-500"
+                    to="/events/hosting">
+                        Hosting
+                </NavLink>
+            </li>
+        </>
+    );
+
     return (
         <>
             <ul className="flex border-b">
-                <li className="-mb-px mr-1">
-                    <NavLink
-                        id="attending"
-                        onClick={handleAttendingClick}
-                        className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-jet-stream-900 hover:text-morning-glory-700 font-semibold hover:bg-jet-stream-200 active:bg-jet-stream-900 target:bg-jet-stream-900 active:text-jet-stream-500 target:text-jet-stream-50"
-                        to="/events/attending">
-                            Attending
-                    </NavLink>
-                </li>
-                <li className="mr-1">
-                    <NavLink
-                        id="hosting"
-                        onClick={handleHostingClick}
-                        className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-jet-stream-900 hover:text-morning-glory-700 font-semibold hover:bg-jet-stream-200 active:bg-jet-stream-900 target:bg-jet-stream-900 active:text-jet-stream-500 target:text-jet-stream-50"
-                        to="/events/hosting">
-                            Hosting
-                    </NavLink>
-                </li>
+                {(userRole === "attending") ? attending() : hosting()}
             </ul>
         </>
     );
