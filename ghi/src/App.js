871d683771b35fcf15fc6a0be4a3e62f2d123017
Emily Arai <emilyarai@gmail.com>
2023-04-27 17:38:36 -0400

    Fixed footer in Your Events Page

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -11,20 +11,16 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Nav />
-        <div>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-
-            <Route path="events">
-              <Route index element={<EventsPage />} />
-              <Route path="attending" element={<EventsPage />} />
-              <Route path="hosting" element={<EventsPage />} />
-            </Route>
-
-          </Routes>
-        </div>
-        <Footer />
+          <Nav />
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="events">
+                <Route index element={<EventsPage />} />
+                <Route path="attending" element={<EventsPage />} />
+                <Route path="hosting" element={<EventsPage />} />
+              </Route>
+            </Routes>
+          <Footer />
       </BrowserRouter>
     </>
   );
