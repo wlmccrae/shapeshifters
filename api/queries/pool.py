59d2af31c221786ac438fe09e268c516ef327508
Emily Arai <emilyarai@gmail.com>
2023-05-03 13:00:45 -0400

    adding db info from CapRover

--- api/queries/pool.py
+++ api/queries/pool.py
@@ -1,6 +1,6 @@
 import os
 from psycopg_pool import ConnectionPool
-print("PRINTING THE OS>ENVIRON.GET in POOL", os.environ.get("DATABASE_URL", "test"))
+print("PRINTING THE OS>ENVIRON.GET in POOL", os.environ.get("DATABASE_URL", "postgresql://postgres:1475998467590045@srv-captain--shapeshifters-db"))
 
 
-pool = ConnectionPool(conninfo=os.environ.get("DATABASE_URL", "test"))
+pool = ConnectionPool(conninfo=os.environ.get("DATABASE_URL", "postgresql://postgres:1475998467590045@srv-captain--shapeshifters-db"))
