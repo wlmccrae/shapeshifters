9d576aea9fdf04bdd7760ebbc3275576003fc5e5
Emily Arai <emilyarai@gmail.com>
2023-05-03 12:50:47 -0400

    Adding print statement to pool for debugging

--- api/queries/pool.py
+++ api/queries/pool.py
@@ -1,4 +1,6 @@
 import os
 from psycopg_pool import ConnectionPool
+print("PRINTING THE OS>ENVIRON.GET in POOL", os.environ.get("DATABASE_URL", "test"))
+
 
 pool = ConnectionPool(conninfo=os.environ.get("DATABASE_URL", "test"))
