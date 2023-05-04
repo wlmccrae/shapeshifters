11ad9a893c1955ab5ad5dc4fb5f3be2253603bfa
Emily Arai <emilyarai@gmail.com>
2023-05-04 15:58:17 -0400

    removed db_script from Dockerfile

--- api/main.py
+++ api/main.py
@@ -11,10 +11,14 @@ app.include_router(accounts.router, tags=["ACCOUNTS"])
 app.include_router(events.router, tags=["EVENTS"])
 app.include_router(attendees.router, tags=["ATTENDEES"])
 
+origins = [
+    "http://localhost:3000",
+    os.environ.get("CORS_HOST", None),
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
