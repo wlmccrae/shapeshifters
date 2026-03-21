9f3857db53a1cfd5e7c2577131c4aa7640ff19a9
Wanda McCrae <wanda.mccrae@gmail.com>
2026-03-21 00:44:08 -0400

    Fix stale connection pool for Neon database

--- api/queries/pool.py
+++ api/queries/pool.py
@@ -3,4 +3,9 @@ from psycopg_pool import ConnectionPool
 print("PRINTING THE OS>ENVIRON.GET in POOL", os.environ.get("DATABASE_URL", "postgresql://postgres:1475998467590045@srv-captain--shapeshifters-db"))
 
 
-pool = ConnectionPool(conninfo=os.environ.get("DATABASE_URL", "postgresql://postgres:1475998467590045@srv-captain--shapeshifters-db"))
+pool = ConnectionPool(
+    conninfo=os.environ.get("DATABASE_URL", "postgresql://postgres:1475998467590045@srv-captain--shapeshifters-db"),
+    kwargs={"autocommit": True},
+    max_waiting=10,
+    reconnect_timeout=30,
+)
